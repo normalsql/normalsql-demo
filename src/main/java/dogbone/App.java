1b8033900d52ef32bb9719503c669213d8467d0c
Jason Osgood <jason@jasonosgood.com>
2024-09-10 09:46:39 -0700

    snapshot

--- src/main/java/dogbone/App.java
+++ src/main/java/dogbone/App.java
@@ -7,7 +7,11 @@ package dogbone;
 public class App 
 {
     public static void main( String[] args )
+        throws Exception
     {
         System.out.println( "Hello World!" );
+//        Class.forName( "org.h2.Driver" );
+        Class.forName( "com.mysql.jdbc.Driver" );
+//        Class.forName( "org.h2.Driver" );
     }
 }
