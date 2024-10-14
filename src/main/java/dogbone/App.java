c4a7deee2656243ced3b98424dca7a810dfba52f
jasonosgood <jason@jasonosgood.com>
2024-10-14 08:02:37 -0700

    snapshot

--- src/main/java/dogbone/App.java
+++ src/main/java/dogbone/App.java
@@ -1,6 +1,6 @@
 package dogbone;
 
-import petclinic.PetsReport;
+//import petclinic.PetsReport;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -18,13 +18,25 @@ public class App
 
         Connection conn = DriverManager.getConnection( "jdbc:h2:tcp://localhost/petclinic-jdbc", null, null );
 
-        var report = new PetsReport( conn );
-        report.setLast_Name( "%" );
-        var reportRS = report.execute();
-        while( reportRS.hasNext() )
-        {
-            System.out.println( reportRS.toString() );
-        }
+        String sql = """
+           SELECT o.Last_Name AS Last, p.Name AS First, t.Name AS Species, Birth_Date AS DOB
+             FROM Owners o
+        LEFT JOIN Pets p ON o.Id = p.Owner_Id
+        LEFT JOIN Types t ON p.Type_Id = T.Id
+                       """;
+
+        var ps = conn.prepareStatement( sql );
+        ps.execute();
+        ps.getResultSet();
+
+
+//        var report = new PetsReport( conn );
+//        report.setLast_Name( "%" );
+//        var reportRS = report.execute();
+//        while( reportRS.hasNext() )
+//        {
+//            System.out.println( reportRS.toString() );
+//        }
 //        SelectOne selectOne = new SelectOne( conn );
 //        SelectOneResultSet ugh = selectOne.execute();
 //        while( ugh.hasNext() )
---
+++ src/main/sql/petclinic/InsertPet.sql
@@ -0,0 +1,4 @@
+INSERT INTO Pets
+    (Name, Birth_Date, Type_Id, Owner_Id)
+VALUES
+    ('ABC', '2020-01-02', 1, 1 );
