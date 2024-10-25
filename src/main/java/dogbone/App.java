b9df423c232daebc413e3e563712aefc25daf21c
Jason Osgood <jason@jasonosgood.com>
2024-10-24 19:40:09 -0700

    snapshot

--- src/main/java/dogbone/App.java
+++ src/main/java/dogbone/App.java
@@ -19,49 +19,43 @@ public class App
             SelectPetsByOwner pets = new SelectPetsByOwner( conn );
         )
         {
-//    {
-            SelectPetsByOwnerResultSet reportRS = pets.execute();
-            while( reportRS.hasNext() )
-            {
-                System.out.println( reportRS );
-                throw new Exception( "whoops" );
-            }
-//    }
+            pets.setLast_Name( "D%" );
+
+//            System.out.println( "same original: " + pets.toString().equals( pets.__originalSQL ));
+
+            extracted( pets );
+
+            var inserter = new InsertPet( conn );
+            inserter.setName( "Dolly" );
+            Date now = new Date( System.currentTimeMillis() );
+            inserter.setBirth_Date( now );
+            var petID = inserter.execute();
+
+            extracted( pets );
+
+            var updater = new UpdatePet( conn );
+            updater.setID( petID );
+            var updateCount = updater.execute();
+
+            extracted( pets );
+
+            var deleter = new DeletePet( conn );
+            deleter.setID( petID );
+            var deleteCount = deleter.execute();
+            extracted( pets );
+
+
         }
-
-//        var report = new PetsReport( conn );
-//        report.setLast_Name( "Frank%" );
-//
-//        extracted( report );
-//
-//        var inserter = new InsertPet( conn );
-//        inserter.setName( "Dolly" );
-//        Date now = new Date( System.currentTimeMillis() );
-//        inserter.setBirth_Date( now );
-//        var petID = inserter.execute();
-//
-//        extracted( report );
-//
-//        var updater = new UpdatePet( conn );
-//        updater.setID( petID );
-//        var updateCount = updater.execute();
-//
-//        extracted( report );
-//
-//        var deleter = new DeletePet( conn );
-//        deleter.setID( petID );
-//        var deleteCount = deleter.execute();
-//        extracted( report );
-
     }
 
-//    public static void extracted( PetsReport report )
-//        throws SQLException
-//    {
-//        var reportRS = report.execute();
-//        while( reportRS.hasNext() )
-//        {
-//            System.out.println( reportRS );
-//        }
-//    }
+    public static void extracted( SelectPetsByOwner pets )
+        throws SQLException
+    {
+//        pets.setLast_Name( "Frank%" );
+        var reportRS = pets.execute();
+        while( reportRS.hasNext() )
+        {
+            System.out.println( reportRS );
+        }
+    }
 }
